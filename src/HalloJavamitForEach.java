2d036009bdd69c42ce14ccac36ebc8588ec2b425
Leonie Oberroither <Leonie Oberroither>
2022-09-29 09:33:29 +0200

    main mehtode

--- src/HalloJavamitForEach.java
+++ src/HalloJavamitForEach.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class HalloJavamitForEach {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class HalloJavamitForEach {
+
+    private List<String> liste;
+
+    public void main(String args[])
+    {
+        liste = Arrays.asList();
+        liste.forEach((String s) -> System.out.println(s));
+    }
+
+
+   
 }

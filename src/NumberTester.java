d57d4b544b46857529873617e708cf24ad4860ad
Leonie Oberroither <Leonie Oberroither>
2022-09-29 09:42:57 +0200

    NumberTest Klassen angelegt

--- src/NumberTester.java
+++ src/NumberTester.java
@@ -1,8 +1,21 @@
 public class NumberTester {
+
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+
     public NumberTester(String fileName) {
     }
 
-    public void setOddEvenTester(NumberTester oddTester){
+    public void setOddEvenTester(NumberTest oddTester){
         this.oddTester = oddTester;
     }
+
+    public void setPrimeTester(NumberTest primeTester){
+        this.primeTester = primeTester;
+    }
+
+    public void testFile(){
+        
+    }
+
 }

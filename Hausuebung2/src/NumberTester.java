e17e40aba713789a327d1ad6f9755591cfb6e81b
Leonie Oberroither <Leonie Oberroither>
2022-10-05 17:05:27 +0200

    NumberTester fertig gemacht und ComplexCalculator geaddet

--- Hausuebung2/src/NumberTester.java
+++ Hausuebung2/src/NumberTester.java
@@ -1,21 +1,77 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class NumberTester {
 
     private NumberTest oddTester;
-    private NumberTest primeTester;
+    private NumberTest printTester;
+    private NumberTest paindroneTester;
+
+    private String[] s;
+
 
     public NumberTester(String fileName) {
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(new File(fileName));
+        } catch (FileNotFoundException e) {
+            System.out.println("Datei existiert nicht");
+            System.exit(0);
+        }
+
+        //bei den Lambdas habe ich mir von einem Klassenkollegen helfen lassen
+
+        setOddTester((int n) -> {return(n % 2) == 0;}) ;
+
+        setPrintTester((int n) -> {if (n ==2 || n == 3 || (n % 2) != 0 && (n % 3) != 0){return true;}
+            return false;});
+
+        setPaindroneTester((int n) -> {
+            Integer i=n;
+            return new StringBuilder(i.toString()).reverse().toString().equals(i.toString());
+        });
     }
 
-    public void setOddEvenTester(NumberTest oddTester){
+    public void setOddTester(NumberTest oddTester) {
         this.oddTester = oddTester;
     }
 
-    public void setPrimeTester(NumberTest primeTester){
-        this.primeTester = primeTester;
+    public void setPrintTester(NumberTest printTester) {
+        this.printTester = printTester;
     }
 
-    public void testFile(){
+    public void setPaindroneTester(NumberTest paindroneTester) {
+        this.paindroneTester = paindroneTester;
+    }
 
+    public void TestFile() {
+        for (String line : s) {
+
+            String[] split = line.split(",");
+
+            if (split[0].equals("1")) {
+                if (oddTester.testNumber(Integer.parseInt(split[1]))) {
+                    System.out.println("EVEN");
+                } else {
+                    System.out.println("NOT EVEN");
+                }
+            } else if (split[0].equals("2")) {
+                if (printTester.testNumber(Integer.parseInt(split[1]))) {
+                    System.out.println("IS PRIME");
+                } else {
+                    System.out.println("NO PRIME");
+                }
+            } else {
+                if (paindroneTester.testNumber(Integer.parseInt(split[1]))) {
+                    System.out.println("PAINDRONE");
+                } else {
+                    System.out.println("NO PAINDRONE");
+                }
+            }
+
+        }
     }
 
 }
--- Hausuebung2/src/RationalCalculator.java
+++ Hausuebung2/src/RationalCalculator.java
@@ -1,2 +1,26 @@
-public class RationalCalculator {
+public class RationalCalculator extends AbstractCalculator {
+
+    public RationalCalculator(CalculateOperation add, CalculateOperation substract, CalculateOperation multiply, CalculateOperation divide) {
+        super(add, substract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        return add.calc(a,b);
+    }
+
+    @Override
+    public Number substract(Number a, Number b) {
+        return substract.calc(a, b);
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return multiply.calc(a, b);
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return divide.calc(a, b);
+    }
 }
